958810c59586f21753a1e5d5e3f5036c64074a9d
Rick <ricleyreis@gmail.com>
2024-07-02 19:58:57 -0300

    FIX - tentando arrumar blank page no deploy da gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio/"
-})
+  base: "/Portfolio",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
+});
