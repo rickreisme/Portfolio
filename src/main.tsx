d46cf0da0439036bc03ea148e19c59e0be9943bc
Rick <ricleyreis@gmail.com>
2024-07-02 19:10:52 -0300

    FIX - configurando rota principal para funcionar no gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -16,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/Portfolio" element={<App />} />
         <Route path="/english" element={<AppEnglish />} />
       </Routes>
     </Router>
