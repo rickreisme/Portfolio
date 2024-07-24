af0a31fbfc4991662e3eefca147e88554e3918d7
Rick <ricleyreis@gmail.com>
2024-07-24 20:53:51 -0300

    FIX - atualizando links de CV

--- src/sessions/Sobre.tsx
+++ src/sessions/Sobre.tsx
@@ -2,39 +2,42 @@ import "../assets/styles/intro-sobre.scss";
 import { RiExternalLinkFill } from "react-icons/ri";
 
 const Sobre = () => {
-  return (
-    <div>
-      <div className="sobre" id="sobre">
-        <div className="sobre-titulo">
-          <h2 className="h2s">
-            &#123;rickreisme&#47;<span>sobre</span>&#125;
-          </h2>
-        </div>
+    return (
+        <div>
+            <div className="sobre" id="sobre">
+                <div className="sobre-titulo">
+                    <h2 className="h2s">
+                        &#123;rickreisme&#47;<span>sobre</span>&#125;
+                    </h2>
+                </div>
 
-        <div className="paragrafo">
-          <p>
-            Olá, meu nome é Rickson Reis! Sou um desenvolvedor apaixonado pelo
-            que faço! <br />
-            Graduado em Análise e Desenvolvimento de Sistemas, com inglês
-            avançado, tenho habilidades sólidas em HTML, CSS, JavaScript e
-            React.js. Além disso, possuo um excelente domínio em SQL
-            (PostgreSQL, Oracle), Flutter, Docker, API REST, Axios, Bootstrap,
-            ASP.NET Core, Python e Firebase. Te convido a conhecer, nesse
-            portfólio, um pouco mais da minha experiência e habilidades.
-          </p>
+                <div className="paragrafo">
+                    <p>
+                        Olá, meu nome é Rickson Reis! Sou um desenvolvedor
+                        apaixonado pelo que faço! <br />
+                        Graduado em Análise e Desenvolvimento de Sistemas, com
+                        inglês avançado, tenho habilidades sólidas em HTML, CSS,
+                        JavaScript e React.js. Além disso, possuo um excelente
+                        domínio em SQL (PostgreSQL, Oracle, SQL Server),
+                        Flutter, Docker, API REST, Axios, Bootstrap, ASP.NET
+                        Core, Python e Firebase. Tenho facilidade para resolver
+                        problemas e sou um aprendiz rápido. Te convido a
+                        conhecer, nesse portfólio, um pouco mais da minha
+                        experiência e habilidades.
+                    </p>
 
-          <a
-            className="btn"
-            href="https://flowcv.com/resume/di16gfcff3"
-            target="_blank"
-          >
-            <RiExternalLinkFill className="link-icon" />
-            <h2>Ver currículo</h2>
-          </a>
+                    <a
+                        className="btn"
+                        href="https://drive.google.com/file/d/1JqwzlRzxOmRHyj4rFPqQ4ceiSdLMRNB5/view?usp=sharing"
+                        target="_blank"
+                    >
+                        <RiExternalLinkFill className="link-icon" />
+                        <h2>Ver currículo</h2>
+                    </a>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Sobre;
--- src/sessions/english/SobreEnglish.tsx
+++ src/sessions/english/SobreEnglish.tsx
@@ -2,39 +2,42 @@ import "../../assets/styles/intro-sobre.scss";
 import { RiExternalLinkFill } from "react-icons/ri";
 
 const SobreEnglish = () => {
-  return (
-    <div>
-      <div className="sobre" id="sobre">
-        <div className="sobre-titulo">
-          <h2 className="h2s">
-            &#123;rickreisme&#47;<span>about</span>&#125;
-          </h2>
-        </div>
+    return (
+        <div>
+            <div className="sobre" id="sobre">
+                <div className="sobre-titulo">
+                    <h2 className="h2s">
+                        &#123;rickreisme&#47;<span>about</span>&#125;
+                    </h2>
+                </div>
 
-        <div className="paragrafo">
-          <p>
-            Hello, my name is Rickson Reis! I am a developer passionate about
-            what I do! <br />
-            Graduated in Systems Analysis and Development, I'm an advanced
-            english speaker, I have solid expertise in HTML, CSS, JavaScript,
-            and React.js. Additionally, I have excellent command of SQL
-            (PostgreSQL, Oracle), Flutter, Docker, REST APIs, Axios, Bootstrap,
-            ASP.NET Core, Python and Firebase. I invite you to explore this
-            portfolio to learn more about my experience and skills.
-          </p>
+                <div className="paragrafo">
+                    <p>
+                        Hello, my name is Rickson Reis! I am a developer
+                        passionate about what I do! <br />
+                        Graduated in Systems Analysis and Development, I'm an
+                        advanced english speaker, I have solid expertise in
+                        HTML, CSS, JavaScript, and React.js. Additionally, I
+                        have excellent command of SQL (PostgreSQL, Oracle, SQL
+                        Server), Flutter, Docker, REST APIs, Axios, Bootstrap,
+                        ASP.NET Core, Python and Firebase. I have the ability to
+                        solve problems and am a quick learner. I invite you to
+                        explore this portfolio to learn more about my experience
+                        and skills.
+                    </p>
 
-          <a
-            className="btn"
-            href="https://drive.google.com/file/d/1l2M9wv8T3dlI6t6YFni10DVxrAKkfRBB/view?usp=drive_link"
-            target="_blank"
-          >
-            <RiExternalLinkFill className="link-icon" />
-            <h2>See my CV</h2>
-          </a>
+                    <a
+                        className="btn"
+                        href="https://drive.google.com/file/d/1l2M9wv8T3dlI6t6YFni10DVxrAKkfRBB/view?usp=drive_link"
+                        target="_blank"
+                    >
+                        <RiExternalLinkFill className="link-icon" />
+                        <h2>See my CV</h2>
+                    </a>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default SobreEnglish;
