1b123dddd0928d953581a424ada9a63987e54ccf
Rick <ricleyreis@gmail.com>
2024-08-26 22:17:20 -0300

    FIX: ajustando

--- src/sessions/english/IntroEnglish.tsx
+++ src/sessions/english/IntroEnglish.tsx
@@ -1,20 +1,20 @@
-import "../../assets/styles/intro-sobre.scss"
-import imgIntro from "../../assets/Imagens/rick-sobre3.png"
+import "../../assets/styles/intro-sobre.scss";
+import imgIntro from "../../assets/Imagens/rick-sobre3.png";
 
 const IntroEnglish = () => {
     return (
-        <div className='intro' id='intro'>
-            <div className='saudacao'>
+        <div className="intro" id="intro">
+            <div className="saudacao">
                 <h1>Rick Reis</h1>
                 <p>Developer</p>
                 <h5>React | PostgreSQL | TypeScript | Node.js | Flutter</h5>
             </div>
 
-            <div className='imagem-intro'>
+            <div className="imagem-intro">
                 <img src={imgIntro}></img>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default IntroEnglish
+export default IntroEnglish;
