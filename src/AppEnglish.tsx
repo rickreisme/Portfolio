7efc8bf379423ceccc7a96f546aa4283cad010e8
Rick <ricleyreis@gmail.com>
2024-10-25 23:11:04 -0300

    REFACTOR: mudando título do site

--- src/AppEnglish.tsx
+++ src/AppEnglish.tsx
@@ -1,4 +1,5 @@
 import "./assets/styles/global.scss";
+// import { Helmet } from "react-helmet-async";
 import ContatoEnglish from "./sessions/english/ContatoEnglish..tsx";
 import ExperienciaEnglish from "./sessions/english/HabilidadesEnglish.tsx";
 import FooterEnglish from "./sessions/english/FooterEnglish.tsx";
@@ -8,22 +9,25 @@ import ProjetosEnglish from "./sessions/english/ProjetosEnglish.tsx";
 import SobreEnglish from "./sessions/english/SobreEnglish.tsx";
 
 function AppEnglish() {
-  return (
-    <>
-      <div className="page-container">
-        <HeaderEnglish />
+    return (
+        <>
+            {/* <Helmet>
+                <title>Rick Reis | Developer </title>
+            </Helmet> */}
+            <div className="page-container">
+                <HeaderEnglish />
 
-        <main>
-          <IntroEnglish />
-          <SobreEnglish />
-          <ExperienciaEnglish />
-          <ProjetosEnglish />
-          <ContatoEnglish />
-          <FooterEnglish />
-        </main>
-      </div>
-    </>
-  );
+                <main>
+                    <IntroEnglish />
+                    <SobreEnglish />
+                    <ExperienciaEnglish />
+                    <ProjetosEnglish />
+                    <ContatoEnglish />
+                    <FooterEnglish />
+                </main>
+            </div>
+        </>
+    );
 }
 
 export default AppEnglish;
